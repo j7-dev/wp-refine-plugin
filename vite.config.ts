21e5b51b11b29e30dae7e0d40b3f56e8c6151742
J7 <j7.dev.gg@outlook.com>
2024-04-03 22:01:49 +0800

    fix: replace deprecated create_config to v4wp

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import create_config from '@kucrut/vite-for-wp'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import alias from '@rollup/plugin-alias'
@@ -6,17 +5,24 @@ import path from 'path'
 
 // import liveReload from 'vite-plugin-live-reload'
 
-export default create_config('js/src/main.tsx', 'js/dist', {
+import { v4wp } from '@kucrut/vite-for-wp'
+
+export default {
   plugins: [
     alias(),
     react(),
     tsconfigPaths(),
 
     // liveReload(__dirname + '/**/*.php'), // Optional, if you want to reload page on php changed
+
+    v4wp({
+      input: 'js/src/main.tsx', // Optional, defaults to 'src/main.js'.
+      outDir: 'js/dist', // Optional, defaults to 'dist'.
+    }),
   ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'js/src'),
     },
   },
-})
+}
