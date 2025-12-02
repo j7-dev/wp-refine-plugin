c2251a6e26ed2e4ea936fd4af1caa78548488e74
J7 <j7.dev.gg@gmail.com>
2025-12-02 14:14:49 +0800

    feat: 改用 pnpm，更新 deps

--- bin/create.cjs
+++ bin/create.cjs
@@ -5,20 +5,20 @@ const { execRemove, execRemoveFile } = require('./utils/remove.cjs')
 const projectName = process?.argv?.[2] || ''
 
 async function main() {
-  // can be shell command or callback function
+	// can be shell command or callback function
 
-  const commands = [
-    `git clone https://github.com/j7-dev/wp-refine-plugin.git ${projectName}`,
-    `cd ${projectName}`,
-    'yarn',
-    `node ./replace-string.cjs ${projectName}`,
-    'composer install --no-interaction',
-    execRemove('.git'),
-    execRemove('bin'),
-    execRemoveFile('replace-string.cjs'),
-    execRemoveFile('README.md'),
-  ]
-  const results = await runCommands(...commands)
+	const commands = [
+		`git clone https://github.com/j7-dev/wp-refine-plugin.git ${projectName}`,
+		`cd ${projectName}`,
+		'pnpm install',
+		`node ./replace-string.cjs ${projectName}`,
+		'composer install --no-interaction',
+		execRemove('.git'),
+		execRemove('bin'),
+		execRemoveFile('replace-string.cjs'),
+		execRemoveFile('README.md'),
+	]
+	const results = await runCommands(...commands)
 }
 
 main()
